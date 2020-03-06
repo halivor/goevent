722d14dfe90af0ead7880a71dbd62cfacb29832c
halivor <hongliang186@aliyun.com>
2020-03-06 09:35:39 +0800

    优化获取本地IP

--- net/ip.go
+++ net/ip.go
@@ -5,23 +5,24 @@ import (
 	"os"
 )
 
-func InIpv4() (string, error) {
-	var ip string
+func InIpv4() ([]string, error) {
+	var ips []string
 	switch addrs, err := net.InterfaceAddrs(); {
 	case err != nil:
-		return "", err
+		return nil, err
 	default:
 		for _, address := range addrs {
 			// 检查ip地址判断是否回环地址
 			if ipnet, ok := address.(*net.IPNet); ok {
 				if ipnet.IP.To4() != nil && !ipnet.IP.IsLoopback() {
-					ip = ipnet.IP.To4().String()
+					ip := ipnet.IP.To4().String()
+					ips = append(ips, ip)
 				}
 			}
 		}
-		if len(ip) == 0 {
-			return "", os.ErrNotExist
+		if len(ips) == 0 {
+			return nil, os.ErrNotExist
 		}
 	}
-	return ip, nil
+	return ips, nil
 }
